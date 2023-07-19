6cc04fe125b85bb4817d8793766b6b2475d79f66
Shalenikov Kirill <kshalen@gmail.com>
2023-07-20 00:09:09 +0300

    Update PersonalizedMessage.swift

--- PersonalizedMessage.swift
+++ PersonalizedMessage.swift
@@ -1,5 +1,11 @@
 /*
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
 
+Use conditionals to return the proper message:
+
+case	                  return
+name equals owner	    'Hello boss'
+otherwise	            'Hello guest'
 */
 
 func great(_ name: String, _ owner: String) -> String {
