828c577616e05b53378fb1acbd35dcad0aef8526
Shalenikov Kirill <kshalen@gmail.com>
2023-07-19 13:27:44 +0300

    Create ReturningStrings.swift

---
+++ ReturningStrings.swift
@@ -0,0 +1,6 @@
+/* Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+*/
+
+func greet(_ name: String) -> String {
+  return "Hello, \(name) how are you doing today?"
+}
