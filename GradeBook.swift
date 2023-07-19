6a177c456ca0151f682e19945d4601b6af1918b6
Shalenikov Kirill <kshalen@gmail.com>
2023-07-19 15:09:44 +0300

    Update GradeBook.swift

--- GradeBook.swift
+++ GradeBook.swift
@@ -10,3 +10,22 @@ Numerical Score	Letter Grade
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 */
 
+func getGrade(_ s1: Int, _ s2: Int, _ s3: Int) -> String {
+  
+  var averageScore: Int {
+    (s1 + s2 + s3) / 3
+  }
+  
+  switch averageScore {
+    case 90...100:
+      return "A"
+    case 80..<90:
+      return "B"
+    case 70..<80:
+      return "C"
+    case 60..<70:
+      return "D"
+    default:
+      return "F"
+  }
+}
