afa31fc62597e7dfb0df3786bb71e7c421a080cd
Shalenikov Kirill <kshalen@gmail.com>
2023-07-19 13:28:37 +0300

    Update ReturningStrings.swift

--- ReturningStrings.swift
+++ ReturningStrings.swift
@@ -1,4 +1,6 @@
-/* Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+/* 
+Make a function that will return a greeting statement that uses an input.
+Your program should return, "Hello, <name> how are you doing today?"
 */
 
 func greet(_ name: String) -> String {
